b7f3eb5f4f77188a1ec7b8a3b6880c674798903d
Trey Shugart <treshugart@gmail.com>
2015-10-24 11:31:00 +1100

    Merge classname binding into this repo.

---
+++ src/classname.js
@@ -0,0 +1,39 @@
+function getClassList (element) {
+  var classList = element.classList;
+  if (classList) {
+    return classList;
+  }
+  var className = element.attributes['class'];
+  className = className && (className.value || className.nodeValue) || '';
+  return (className && className.split(/\s+/)) || [];
+}
+
+export default {
+  create (opts) {
+    var elem = document.createElement(opts.extends || 'div');
+    elem.className = opts.id;
+    return elem;
+  },
+  filter (elem, defs) {
+    var classList = getClassList(elem);
+    var classListLen = classList.length;
+    var definitions = [];
+    var tag = (elem.tagName || elem.localName).toLowerCase();
+
+    for (let a = 0; a < classListLen; a++) {
+      let className = classList[a];
+      let definition = defs[className];
+
+      if (!definition) {
+        continue;
+      }
+
+      let tagToExtend = definition.extends;
+      if (!tagToExtend || tag === tagToExtend) {
+        definitions.push(definition);
+      }
+    }
+
+    return definitions;
+  }
+};
--- src/index.js
+++ src/index.js
@@ -1,29 +1,7 @@
+import attribute from './attribute';
+import classname from './classname';
+
 export default {
-  create (opts) {
-    var elem = document.createElement(opts.extends || 'div');
-    elem.setAttribute(opts.id, '');
-    return elem;
-  },
-  filter (elem, defs) {
-    var attrs = elem.attributes;
-    var attrsLen = attrs.length;
-    var definitions = [];
-    var tag = (elem.tagName || elem.localName).toLowerCase();
-
-    for (let a = 0; a < attrsLen; a++) {
-      let attr = attrs[a].nodeName;
-      let definition = defs[attr];
-
-      if (!definition) {
-        continue;
-      }
-
-      let tagToExtend = definition.extends;
-      if (!tagToExtend || tag === tagToExtend) {
-        definitions.push(definition);
-      }
-    }
-
-    return definitions;
-  }
+  attribute: attribute,
+  classname: classname
 };
