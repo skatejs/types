f54e06cca070d0ec431ba3d066864cf27e0bc6e1
Joshua Nelson <jonelson@atlassian.com>
2016-04-18 11:48:41 +1000

    Rebuild

--- dist/index.js
+++ dist/index.js
@@ -1,164 +1,185 @@
-// src/attribute.js
-__54026cc39d66d3aed10e4fe87255cdd3 = (function () {
-  var module = {
-    exports: {}
-  };
-  var exports = module.exports;
-  
-  'use strict';
-  
-  Object.defineProperty(exports, '__esModule', {
-    value: true
-  });
-  exports['default'] = {
-    create: function create(opts) {
-      var elem = document.createElement(opts['extends'] || 'div');
-      elem.setAttribute(opts.id, '');
-      return elem;
-    },
-    filter: function filter(elem, defs) {
-      var attrs = elem.attributes;
-      var attrsLen = attrs.length;
-      var definitions = [];
-      var tag = (elem.tagName || elem.localName).toLowerCase();
-  
-      for (var a = 0; a < attrsLen; a++) {
-        var attr = attrs[a].nodeName;
-        var definition = defs[attr];
-  
-        if (!definition) {
-          continue;
-        }
-  
-        var tagToExtend = definition['extends'];
-        if (!tagToExtend || tag === tagToExtend) {
-          definitions.push(definition);
-        }
+(function (global, factory) {
+  typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
+  typeof define === 'function' && define.amd ? define(factory) :
+  (global.skatejsTypes = factory());
+}(this, function () {
+
+  var __commonjs_global = typeof window !== 'undefined' ? window : typeof global !== 'undefined' ? global : this;
+  function __commonjs(fn, module) { return module = { exports: {} }, fn(module, module.exports, __commonjs_global), module.exports; }
+
+  var classname = __commonjs(function (module, exports, global) {
+  (function (global, factory) {
+    if (typeof define === "function" && define.amd) {
+      define(['module', 'exports'], factory);
+    } else if (typeof exports !== "undefined") {
+      factory(module, exports);
+    } else {
+      var mod = {
+        exports: {}
+      };
+      factory(mod, mod.exports);
+      global.classname = mod.exports;
+    }
+  })(__commonjs_global, function (module, exports) {
+    'use strict';
+
+    Object.defineProperty(exports, "__esModule", {
+      value: true
+    });
+    function getClassList(element) {
+      var classList = element.classList;
+      if (classList) {
+        return classList;
       }
-  
-      return definitions;
+      var className = element.attributes['class'];
+      className = className && (className.value || className.nodeValue) || '';
+      return className && className.split(/\s+/) || [];
     }
-  };
-  module.exports = exports['default'];
-  
-  return module.exports;
-}).call(this);
-// src/classname.js
-__3234d8f48147f132a33e6e88b1b16f31 = (function () {
-  var module = {
-    exports: {}
-  };
-  var exports = module.exports;
-  
-  'use strict';
-  
-  Object.defineProperty(exports, '__esModule', {
-    value: true
-  });
-  function getClassList(element) {
-    var classList = element.classList;
-    if (classList) {
-      return classList;
-    }
-    var className = element.attributes['class'];
-    className = className && (className.value || className.nodeValue) || '';
-    return className && className.split(/\s+/) || [];
-  }
-  
-  exports['default'] = {
-    create: function create(opts) {
-      var elem = document.createElement(opts['extends'] || 'div');
-      elem.className = opts.id;
-      return elem;
-    },
-    filter: function filter(elem, defs) {
-      var classList = getClassList(elem);
-      var classListLen = classList.length;
-      var definitions = [];
-      var tag = (elem.tagName || elem.localName).toLowerCase();
-  
-      for (var a = 0; a < classListLen; a++) {
-        var className = classList[a];
-        var definition = defs[className];
-  
-        if (!definition) {
-          continue;
+
+    exports.default = {
+      create: function create(opts) {
+        var elem = document.createElement(opts.extends || 'div');
+        elem.className = opts.id;
+        return elem;
+      },
+      reduce: function reduce(elem, defs) {
+        var classList = getClassList(elem);
+        var classListLen = classList.length;
+        var tag = (elem.tagName || elem.localName).toLowerCase();
+
+        for (var a = 0; a < classListLen; a++) {
+          var className = classList[a];
+          var definition = defs[className];
+
+          if (!definition) {
+            continue;
+          }
+
+          var tagToExtend = definition.extends;
+          if (!tagToExtend || tag === tagToExtend) {
+            return definition;
+          }
         }
-  
-        var tagToExtend = definition['extends'];
-        if (!tagToExtend || tag === tagToExtend) {
-          definitions.push(definition);
-        }
+
+        return false;
       }
-  
-      return definitions;
+    };
+    module.exports = exports['default'];
+  });
+  });
+
+  var require$$0 = (classname && typeof classname === 'object' && 'default' in classname ? classname['default'] : classname);
+
+  var attribute = __commonjs(function (module, exports, global) {
+  (function (global, factory) {
+    if (typeof define === "function" && define.amd) {
+      define(['module', 'exports'], factory);
+    } else if (typeof exports !== "undefined") {
+      factory(module, exports);
+    } else {
+      var mod = {
+        exports: {}
+      };
+      factory(mod, mod.exports);
+      global.attribute = mod.exports;
     }
-  };
-  module.exports = exports['default'];
-  
-  return module.exports;
-}).call(this);
-// src/index.js
-__ba05207d5a5325eca46834e0ad2b8428 = (function () {
-  var module = {
-    exports: {}
-  };
-  var exports = module.exports;
-  
-  'use strict';
-  
-  Object.defineProperty(exports, '__esModule', {
-    value: true
+  })(__commonjs_global, function (module, exports) {
+    'use strict';
+
+    Object.defineProperty(exports, "__esModule", {
+      value: true
+    });
+    exports.default = {
+      create: function create(opts) {
+        var elem = document.createElement(opts.extends || 'div');
+        elem.setAttribute(opts.id, '');
+        return elem;
+      },
+      reduce: function reduce(elem, defs) {
+        var attrs = elem.attributes;
+        var attrsLen = attrs.length;
+        var tag = (elem.tagName || elem.localName).toLowerCase();
+
+        for (var a = 0; a < attrsLen; a++) {
+          var attr = attrs[a].nodeName;
+          var definition = defs[attr];
+
+          if (!definition) {
+            continue;
+          }
+
+          var tagToExtend = definition.extends;
+          if (!tagToExtend || tag === tagToExtend) {
+            return definition;
+          }
+        }
+
+        return false;
+      }
+    };
+    module.exports = exports['default'];
   });
-  
-  function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { 'default': obj }; }
-  
-  var _attribute = __54026cc39d66d3aed10e4fe87255cdd3;
-  
-  var _attribute2 = _interopRequireDefault(_attribute);
-  
-  var _classname = __3234d8f48147f132a33e6e88b1b16f31;
-  
-  var _classname2 = _interopRequireDefault(_classname);
-  
-  exports['default'] = {
-    attribute: _attribute2['default'],
-    classname: _classname2['default']
-  };
-  module.exports = exports['default'];
-  
-  return module.exports;
-}).call(this);
-// src/global.js
-__d1542c3ec42bede5f7c7a7513bed47ad = (function () {
-  var module = {
-    exports: {}
-  };
-  var exports = module.exports;
-  
-  'use strict';
-  
-  Object.defineProperty(exports, '__esModule', {
-    value: true
   });
-  
-  function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { 'default': obj }; }
-  
-  var _index = __ba05207d5a5325eca46834e0ad2b8428;
-  
-  var _index2 = _interopRequireDefault(_index);
-  
-  var previousSkateTypes = window.skateTypes;
-  function noConflict() {
-    window.skateTypes = previousSkateTypes;
+
+  var require$$1 = (attribute && typeof attribute === 'object' && 'default' in attribute ? attribute['default'] : attribute);
+
+  var index = __commonjs(function (module, exports, global) {
+  (function (global, factory) {
+    if (typeof define === "function" && define.amd) {
+      define(['module', 'exports', './attribute', './classname'], factory);
+    } else if (typeof exports !== "undefined") {
+      factory(module, exports, require$$1, require$$0);
+    } else {
+      var mod = {
+        exports: {}
+      };
+      factory(mod, mod.exports, global.attribute, global.classname);
+      global.index = mod.exports;
+    }
+  })(__commonjs_global, function (module, exports, _attribute, _classname) {
+    'use strict';
+
+    Object.defineProperty(exports, "__esModule", {
+      value: true
+    });
+
+    var _attribute2 = _interopRequireDefault(_attribute);
+
+    var _classname2 = _interopRequireDefault(_classname);
+
+    function _interopRequireDefault(obj) {
+      return obj && obj.__esModule ? obj : {
+        default: obj
+      };
+    }
+
+    exports.default = {
+      attribute: _attribute2.default,
+      classname: _classname2.default
+    };
+    module.exports = exports['default'];
+  });
+  });
+
+  var main = (index && typeof index === 'object' && 'default' in index ? index['default'] : index);
+
+
+  var api = Object.freeze({
+    default: main
+  });
+
+  var previousGlobal = window.skatejsTypes;
+  main.noConflict = function noConflict() {
+    window.skatejsTypes = previousGlobal;
     return this;
+  };
+  window.skatejsTypes = main;
+  for (var name in api) {
+    main[name] = api[name];
   }
-  
-  _index2['default'].noConflict = noConflict;
-  window.skateTypes = _index2['default'];
-  
-  exports['default'] = _index2['default'];
-  module.exports = exports['default'];
-  
-  return module.exports;
-}).call(this);
+  main.version = '0.0.0';
+
+  return main;
+
+}));
+//# sourceMappingURL=index.js.map
